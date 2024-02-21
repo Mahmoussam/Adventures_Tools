28989c8009f1730aa27b35ee561cc3c8b31c0dfe
Mahmoussam <51446585+Mahmoussam@users.noreply.github.com>
2024-02-21 22:21:16 +0200

    Update robowser.py

--- robowser.py
+++ robowser.py
@@ -5,11 +5,11 @@ from selenium.webdriver.edge.service import Service as EdgeService
 edge_driver_path = "edgedriver_w64/msedgedriver.exe"  
 edge_options = webdriver.EdgeOptions()
 edge_options.use_chromium = True  # This is required for recent versions of Microsoft Edge
-uag="Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/10.0 b9ad90fa05be5e6113ebfc512afd23ac732ec3716a3e4c3b95b86dce789f6f6a/2.0.0.1)"
+uag="Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/10.0 b8asd98ads8a7fhhgj/2.0.0.1)"
 edge_options.add_argument("user-agent="+"apachiano")
 edge_options.add_argument("GUID="+"4440-1321-1321-9797")
 
 # Create a new instance of the Edge driver with the specified options
 driver = webdriver.Edge(service=EdgeService(edge_driver_path), options=edge_options)
 
-driver.get("https://v231w.mrahmedsamir-app.com/")
+driver.get("https://site-to-test.com")
